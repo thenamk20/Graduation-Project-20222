d0ab27fa29df251c2db9ebc47dfa2dc048e9c46f
Pham The Nam <nampt@rocketstudio.com.vn>
2023-07-04 19:48:21 +0700

    room item avatar

--- Assets/Scripts/HyperCatScripts/UI/Components/Room/PlayerRoomItem.cs
+++ Assets/Scripts/HyperCatScripts/UI/Components/Room/PlayerRoomItem.cs
@@ -5,22 +5,30 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 
 public class PlayerRoomItem : MonoBehaviourPunCallbacks
 {
     [SerializeField] private TextMeshProUGUI playerName;
 
+    [SerializeField] private TextMeshProUGUI avatarIndexText;
+
     private Player player;
-     
+
     public void Init(Player _player)
     {
         player = _player;
         playerName.text = _player.NickName;
+
+        if (_player == PhotonNetwork.LocalPlayer)
+        {
+            SetAvatar(GameManager.Instance.data.user.userRemoteData.avatarID);
+        }
     }
 
     public override void OnPlayerLeftRoom(Player otherPlayer)
     {
-        if(player == otherPlayer)
+        if (player == otherPlayer)
         {
             Destroy(gameObject);
             EventGlobalManager.Instance.OnPlayersRoomChange.Dispatch();
@@ -30,5 +38,31 @@ public class PlayerRoomItem : MonoBehaviourPunCallbacks
     public override void OnLeftRoom()
     {
         Destroy(gameObject);
+    }
+
+    public void SetAvatar(int avatarIndex)
+    {
+        Hashtable hash = new Hashtable
+        {
+            { "avatar", avatarIndex }
+        };
+
+        HCDebug.Log(hash);
+
+        PhotonNetwork.LocalPlayer.SetCustomProperties(hash);
+    }
+
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
+    {
+        if(player == targetPlayer)
+        {
+            if (changedProps.ContainsKey("avatar"))
+            {
+                if (changedProps.TryGetValue("avatar", out object avatar))
+                {
+                    avatarIndexText.text = ((int)avatar).ToString();
+                }
+            }
+        }
     }
 }
